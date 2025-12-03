cdee6fc04f1cd223ae65d8996f8561ec52d4d9ea
ctunbridge <c.tunbridge@live.co.uk>
2025-12-03 16:46:17 +0000

    tweak: outline button hover over

--- components/ui/button/button.tsx
+++ components/ui/button/button.tsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20",
+          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20",
         outline:
-          "border bg-background shadow-xs hover:text-secondary",
+          "border bg-background hover:bg-muted/20",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
           "hover:muted-foreground hover:text-secondary",
         link: "text-primary underline-offset-4 hover:underline",
